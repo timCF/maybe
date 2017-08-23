06d5459f5a2db23e4e614608442d1112264a3ffb
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2017-08-23 13:44:21 +0300

    rm warning for modern elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Maybe.Mixfile do
     [app: :maybe,
      version: "0.0.1",
      #elixir: "~> 1.1.0",
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
